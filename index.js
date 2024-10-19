66419a0cd3f161101a07607a5589a30e1dfa9324
Artemis <clare.stock@gmail.com>
2024-10-19 11:36:27 -0400

    changes regarding bot responses

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, ChannelType } = require('discord.js');
 const { prefix } = require('./config.json');
-
-const token = process.env['DISCORD_TOKEN']
+require('dotenv').config();
+const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping
+    ],
+    Partials: [
+        Partials.Channel,
+        Partials.Message
+    ]
 });
 
 client.once('ready', () =>{
     console.log('Bot is Online!');
 });
 
-client.on('messageCreate', message =>{
+client.on('messageCreate', (message)=>{
     if(message.author.bot) return;
 
-    if(message.content.startsWith(prefix)){
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-
-        if(command === 'ping'){
-            message.channel.send('Pong!');
-        }
-
-        if(command === 'hello'){
-            message.channel.send('Hiya!');
-        }
+    console.log(`message received: ${message.content}`)
+    if(message.channel.type === ChannelType.DM){
+        console.log('DM detected');
+        message.reply('I am a bot, please use channel for commands.');
     }
+
+    // if(message.content.startsWith(prefix)){
+    //     const args = message.content.slice(prefix.length).trim().split(/ +/);
+    //     const command = args.shift().toLowerCase();
+
+    //     if(command === 'ping'){
+    //         message.channel.send('Pong!');
+    //     }
+
+    //     if(command === 'hello'){
+    //         message.channel.send('Hiya!');
+    //     }
+    // }
 
 
 });
